6e5476b7146e45ddfa6018e5bb5351af48aa78fc
SeungWoo <wjknn3123@gmail.com>
2023-10-18 20:25:54 +0900

    ADD : english data

---
+++ src/assets/english/lesson4.ts
@@ -0,0 +1,114 @@
+import { englishType } from 'types'
+
+export const lesson4: englishType[] = [
+  {
+    id: 1,
+    word: 'application',
+    meaning: ['지원', '신청'],
+  },
+  {
+    id: 2,
+    word: 'be eager to',
+    meaning: ['간절히 바라다'],
+  },
+  {
+    id: 3,
+    word: 'bond',
+    meaning: ['접착', '유대'],
+  },
+  {
+    id: 4,
+    word: 'charity',
+    meaning: ['자선단체'],
+  },
+  {
+    id: 5,
+    word: 'conflict',
+    meaning: ['갈등', '충돌'],
+  },
+  {
+    id: 6,
+    word: 'costume',
+    meaning: ['의상'],
+  },
+  {
+    id: 7,
+    word: 'decrease',
+    meaning: ['줄어들다', '감소하다'],
+  },
+  {
+    id: 8,
+    word: 'disadvantaged',
+    meaning: ['혜택을 받지 못하는', '빈곤한'],
+  },
+  {
+    id: 9,
+    word: 'distribute',
+    meaning: ['분배하다'],
+  },
+  {
+    id: 10,
+    word: 'entry fee',
+    meaning: ['참가비', '가입비'],
+  },
+  {
+    id: 11,
+    word: 'fundrasing',
+    meaning: ['모금'],
+  },
+  {
+    id: 12,
+    word: 'individual',
+    meaning: ['개인적인'],
+  },
+  {
+    id: 13,
+    word: 'inexpensive',
+    meaning: ['비싸지 않은'],
+  },
+  {
+    id: 14,
+    word: 'lift',
+    meaning: ['들다', '상황을 나아지게 하다'],
+  },
+  {
+    id: 15,
+    word: 'nonprofit',
+    meaning: ['비영리적인'],
+  },
+  {
+    id: 16,
+    word: 'option',
+    meaning: ['선택'],
+  },
+  {
+    id: 17,
+    word: 'poverty',
+    meaning: ['가난', '빈곤'],
+  },
+  {
+    id: 18,
+    word: 'recommend',
+    meaning: ['추천하다'],
+  },
+  {
+    id: 19,
+    word: 'shelter',
+    meaning: ['쉼터', '보호소'],
+  },
+  {
+    id: 20,
+    word: 'sorrow',
+    meaning: ['슬픈'],
+  },
+  {
+    id: 21,
+    word: 'inspiring',
+    meaning: ['영감을 주는'],
+  },
+  {
+    id: 22,
+    word: 'cooperative',
+    meaning: ['협력하는'],
+  },
+]
---
+++ src/types/englishType.ts
@@ -0,0 +1,5 @@
+export interface englishType {
+  id: number
+  word: string
+  meaning: string[]
+}
