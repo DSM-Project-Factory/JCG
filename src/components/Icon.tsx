b5e9cf4af5b202851f5a53f522d95cac7eade532
SeungWoo <wjknn3123@gmail.com>
2023-10-18 20:25:27 +0900

    EDIT : add clickable in Icon.tsx

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,3 +1,6 @@
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { jsx } from '@emotion/react'
 import { HTMLAttributes } from 'react'
 import { IconName, IconSet, colors, colorsKeyOfType } from 'constant'
 
@@ -5,15 +8,17 @@ interface Props extends HTMLAttributes<HTMLOrSVGElement> {
   name: IconName
   size?: number
   color?: colorsKeyOfType
+  clickable?: boolean
 }
 
-export const Icon = ({ name, size, color, ...props }: Props) => {
+export const Icon = ({ name, size, color, clickable, ...props }: Props) => {
   return (
     <svg
       height={size ?? 24}
       viewBox={IconSet[name].viewBox}
       fill={color && colors[color]}
       xmlns="http://www.w3.org/2000/svg"
+      css={{ cursor: clickable ? 'pointer' : 'default' }}
       {...props}
     >
       <path d={IconSet[name].path} />
