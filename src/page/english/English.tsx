bccfd5ca34226d27096e1d0e938164ea779ce7ef
SeungWoo <wjknn3123@gmail.com>
2023-12-18 12:21:20 +0900

    ADD : recent english

--- src/page/english/English.tsx
+++ src/page/english/English.tsx
@@ -1,43 +1,88 @@
 import styled from '@emotion/styled'
 import { InfoElement, Txt } from 'components'
+import { useEffect, useState } from 'react'
 
-const data = [
+interface dataType {
+  id: number
+  subject: string
+  explanation: string
+  linkTo: string
+}
+
+const data: dataType[][] = [
   [
-    { subject: 'Lesson 5', explanation: 'Our Earth, Our Future, Our Choice', linkTo: '/english/1/5' },
-    { subject: 'Lesson 6', explanation: 'Enjoy Art, Connect Hearts', linkTo: '/english/1/6' },
-    { subject: 'Lesson 7', explanation: 'Science Everywhere', linkTo: '/english/1/7' },
-    { subject: 'Lesson 8', explanation: 'Korean Culture: The Mirror of Our Hearts', linkTo: '/english/1/8' },
+    { id: 15, subject: 'Lesson 5', explanation: 'Our Earth, Our Future, Our Choice', linkTo: '/english/1/5' },
+    { id: 16, subject: 'Lesson 6', explanation: 'Enjoy Art, Connect Hearts', linkTo: '/english/1/6' },
+    { id: 17, subject: 'Lesson 7', explanation: 'Science Everywhere', linkTo: '/english/1/7' },
+    { id: 18, subject: 'Lesson 8', explanation: 'Korean Culture: The Mirror of Our Hearts', linkTo: '/english/1/8' },
   ],
   [
-    { subject: 'Lesson 2', explanation: 'Art Around Us', linkTo: '/english/2/2' },
-    { subject: 'Lesson 3', explanation: 'Toward a Better Environment', linkTo: '/english/2/3' },
-    { subject: 'Lesson 4', explanation: 'Living Together', linkTo: '/english/2/4' },
-    { subject: 'Lesson 5', explanation: 'Cultures as a Mirror of Our Lives', linkTo: '/english/2/5' },
-    { subject: 'Lesson 6', explanation: 'Technology, What Defines Us as Humans', linkTo: '/english/2/6' },
+    { id: 22, subject: 'Lesson 2', explanation: 'Art Around Us', linkTo: '/english/2/2' },
+    { id: 23, subject: 'Lesson 3', explanation: 'Toward a Better Environment', linkTo: '/english/2/3' },
+    { id: 24, subject: 'Lesson 4', explanation: 'Living Together', linkTo: '/english/2/4' },
+    { id: 25, subject: 'Lesson 5', explanation: 'Cultures as a Mirror of Our Lives', linkTo: '/english/2/5' },
+    { id: 26, subject: 'Lesson 6', explanation: 'Technology, What Defines Us as Humans', linkTo: '/english/2/6' },
   ],
   [],
 ]
 
 const English = () => {
+  const [recentEnglish, setRecentEnglish] = useState<dataType[]>([])
+
+  useEffect(() => {
+    const item = localStorage.getItem('recent_english')
+    if (!item) {
+      localStorage.setItem('recent_english', '[]')
+    } else {
+      item && setRecentEnglish(JSON.parse(item))
+    }
+  }, [])
+
+  const addRecentEnglish = (dataIndex: number, index: number) => {
+    const addData = data[dataIndex][index]
+    const english = recentEnglish.filter(v => v.id !== addData.id)
+    const result = [{ ...addData }, ...english]
+    localStorage.setItem('recent_english', JSON.stringify(result))
+    setRecentEnglish(result)
+  }
+
+  const removeRecentEnglish = () => {
+    localStorage.setItem('recent_english', '[]')
+    setRecentEnglish([])
+  }
+
   return (
     <Frame>
+      <div>
+        {recentEnglish.map(v => (
+          <div key={v.id}>{v.explanation}</div>
+        ))}
+      </div>
+      <button onClick={() => removeRecentEnglish()}>최근 기록 초기화</button>
+      <br />
       <Txt typography="h1">English</Txt>
       <ListDiv>
         <Txt typography="h3">1st Grade</Txt>
         {data[0].map((value, index) => (
-          <InfoElement {...value} key={'1st' + index} />
+          <div onClick={() => addRecentEnglish(0, index)} key={'1st' + index}>
+            <InfoElement {...value} />
+          </div>
         ))}
       </ListDiv>
       <ListDiv>
         <Txt typography="h3">2nd Grade</Txt>
         {data[1].map((value, index) => (
-          <InfoElement {...value} key={'2nd' + index} />
+          <div onClick={() => addRecentEnglish(1, index)} key={'2nd' + index}>
+            <InfoElement {...value} />
+          </div>
         ))}
       </ListDiv>
       <ListDiv>
         <Txt typography="h3">3rd Grade</Txt>
         {data[2].map((value, index) => (
-          <InfoElement {...value} key={'3rd' + index} />
+          <div onClick={() => addRecentEnglish(2, index)} key={'3rd' + index}>
+            <InfoElement {...value} />
+          </div>
         ))}
       </ListDiv>
     </Frame>
